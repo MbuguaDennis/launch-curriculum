67057c8ad44659bf2f8a8d309eb4d6868f541f4a
MbuguaDennis <125797368+MbuguaDennis@users.noreply.github.com>
2026-02-01 12:37:45 +0300

    Add: Get started with sdk for NET development

---
+++ module0/MyFirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// Data types
+// Text
+using System.Security;
+
+string firstName = "Dennis";
+ char firstNameLetter = firstName[0];
+
+ int salary = 300000;
+
+string profession = "Software Engineering";
+
+Console.WriteLine(firstName);
+Console.WriteLine(salary);
+Console.WriteLine(profession);
+Console.WriteLine(firstNameLetter);
+
+
+Console.WriteLine($"My name is {firstName}, I work in {profession} and my salary is {salary}");
+
+
+string dayOfWeek = "Sunday";
+bool isAtHome = true;
+
+if (dayOfWeek == "Sunday" && isAtHome)
+
+{
+   Console.WriteLine($"Hey {firstName} today is {dayOfWeek} and you can code from home {isAtHome}"); 
+}
